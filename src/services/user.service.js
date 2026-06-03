cec02a04972d46b1cc9984bb0a52180a32cc8eca
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-06-03 04:59:19 -0700

    followed steps till part 2

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,12 @@
 import db from "../db/db.js";
 
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (plainPassword) => {
+    const saltRounds = 10;
+    return await bcrypt.hash(plainPassword, saltRounds);
+};
+
 export const findUserByUsername = async (username) => {
     const [results] = await db.query(
         "SELECT userId, username, password, role FROM users WHERE username = ? LIMIT 1",
@@ -8,10 +15,19 @@ export const findUserByUsername = async (username) => {
     return results[0];
 };
 
-export const createUser = async (username, password, role = "user") => {
+export const createUser = async (username, plainPassword, role = "user") => {
+
+    if (!username) throw new Error("Username is required.");
+    if (!plainPassword) throw new Error("Password is required.");
+    if (role !== "user" && role !== "admin") throw new Error("Invalid role.");
+
+    //hash the password before insert!
+    const passwordHash = await hashPassword(plainPassword);
+
+
     const [result] = await db.execute(
         "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
-        [username, password, role]
+        [username, passwordHash, role]
     );
 
     return {
